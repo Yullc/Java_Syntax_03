a61eab992b47ea79bf5568b263e8b33262425b23
Yullc <apanan0910@gmail.com>
2025-03-04 16:22:37 +0900

    day 1 for 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,120 +1,210 @@
 package org.example;
 
-// 문제 : 할인 대상인지 아닌지 출력해주세요.
-// 조건 : 나이가 19세 이하이거나 60세 이상이면 할인 대상입니다.
-// 조건 : 출력예시 처럼 출력되어야 합니다.
-// 조건 : `구현시작` 부분만 수정 할 수 있습니다.
-// 조건 : 4가지 이상의 방법으로 풀어야 합니다.
-// 조건 : 그 중 2가지 방법은 `&&, ||`없이 풀어야 합니다.
+// 문제 : 아래와 같이 출력해주세요.
+
+// 문제 : 아래와 같이 출력해주세요.
+// 문제 : 2부터 9까지의 숫자를 사용하지 않고 구구단 출력을 완성 해주세요.
 
 class Main {
     public static void main(String[] args) {
-        
-        int age = 19; // 이 값을 바꿔가면서 실행해보세요.
-        System.out.println("당신의 나이는 " + age + "살 입니다.");
+        int dan = 1;
 
-        if (age <= 19) {
-            System.out.println("할인 대상입니다.");
-        } else if (age >= 60) {
-            System.out.println("할인 대상입니다.");
-        } else
-            if (age > 19) {
-            if (age < 60) {
-                System.out.println("할인 대상이 아닙니다.");
-            }
-        }
+        // 수정가능지역 시작
+        int i =1;
+
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+
+        dan++;
+        i=1;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        dan++;
+        i=1;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+
+        dan++;
+        i=1;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+
+        dan++;
+        i=1;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+
+        dan++;
+        i=1;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+
+        dan++;
+        i=1;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+
+        dan++;
+        i=1;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+
+        dan++;
+        i=1;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
+        i++;
+        System.out.println(dan + " * " + i + " = " + dan * i) ;
 
 
-//        6.
-//        if(age<=19||age>=60){
-//            System.out.println("할인 대상입니다.");
-//
-//        }
-//        else{
-//            System.out.println("할인 대상이 아닙니다.");
-//        }
 
 
-//        5.
-//        if(age<10){
-//            System.out.println("할인 대상입니다.");
-//        }
-//       else if(age<20){
-//            System.out.println("할인 대상이 아닙니다.");
-//        }
-//       else if(age<30){
-//            System.out.println("할인 대상이 아닙니다.");
-//        }
-//        else if(age<40){
-//            System.out.println("할인 대상이 아닙니다.");
-//        }
-//        else if(age<50){
-//            System.out.println("할인 대상이 아닙니다.");
-//        }
-//        else if(age<60){
-//            System.out.println("할인 대상이 아닙니다.");
-//        }
-//        else if(60<age){
-//            System.out.println("할인 대상입니다.");
-//            }
+        // 수정가능지역 끝
 
-        //4.
-//        if(age<10){
-//            System.out.println("할인 대상입니다.");
-//        }
-//       else if(age<20){
-//            System.out.println("할인 대상이 아닙니다.");
-//        }
-//       else if(age<30){
-//            System.out.println("할인 대상이 아닙니다.");
-//        }
-//        else if(age<40){
-//            System.out.println("할인 대상이 아닙니다.");
-//        }
-//        else if(age<50){
-//            System.out.println("할인 대상이 아닙니다.");
-//        }
-//        else if(age<60){
-//            System.out.println("할인 대상이 아닙니다.");
-//        }
-//        else {
-//            System.out.println("할인 대상입니다.");
-//            }
-
-        // 3.
-//        if(age<=19){
-//            System.out.println("할인 대상입니다.");
-//        }
-//        else if(age>=60){
-//            System.out.println("할인 대상입니다.");
-//        }
-//        else {
-//           System.out.println("항인 대상이 아닙니다.");
-//       }
-
-        // 2.
-//        if(age<=19){
-//            System.out.println("할인 대상입니다.");
-//        }
-//        else if(age>=60){
-//            System.out.println("할인 대상입니다.");
-//        }
-//        else if(age>19&&age<60){
-//            System.out.println("항인 대상이 아닙니다.");
-//        }
-
-//        // 구현시작
-//        1.
-//        if(age<=19){
-//            System.out.println("할인 대상입니다.");
-//        }
-//        if(age>=60){
-//            System.out.println("할인 대상입니다.");
-//        }
-//        if(age>19&&age<60){
-//            System.out.println("항인 대상이 아닙니다.");
-//        }
-
-        // 출력 => 할인대상입니다. 또는 할인 대상이 아닙니다.
+        // 출력
+    /*
+    8 * 1 = 8
+    8 * 2 = 16
+    8 * 3 = 24
+    8 * 4 = 32
+    8 * 5 = 40
+    8 * 6 = 48
+    8 * 7 = 56
+    8 * 8 = 64
+    8 * 9 = 72
+    */
     }
 }
