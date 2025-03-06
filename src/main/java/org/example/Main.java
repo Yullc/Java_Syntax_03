db0eccf1bf9b069b3a07061068abd2c386cb3752
Yullc <apanan0910@gmail.com>
2025-03-06 16:58:03 +0900

    day3 codeUp 문제 실패

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,21 @@
 package org.example;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] s = sc.next().split("\\.");
-        System.out.println(s[0]);
-        System.out.println(s[1]);
+        String str = sc.nextLine();
+        String[] time = str.split("");
+        if(str.length()==7){
+            time[2]
+            System.out.println(time[2]+time[3]);
+        }
+        else{
+            System.out.println(time[3]+time[4]);
+        }
+       ;
     }
 }
