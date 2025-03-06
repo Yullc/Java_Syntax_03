703631bfbe36ac9a23e767415f5875e33e36dc38
Yullc <apanan0910@gmail.com>
2025-03-06 14:36:17 +0900

    day3 codeUp 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,34 +1,13 @@
 package org.example;
 
+
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-
-        int n = 4;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j < n; j++) {
-                System.out.print(" ");
-
-            }
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-
-        }
-
-        for (int i = n - 1; i >= 1; i--) {
-            for (int k = n; k > i; k--) {
-                System.out.print(" ");
-            }
-            for (int k =1;  k <= (2 * i - 1); k++) {
-                System.out.print("*");
-
-            }
-            System.out.println();
-        }
-
+        Scanner sc = new Scanner(System.in);
+        String[] s = sc.next().split("\\.");
+        System.out.println(s[0]);
+        System.out.println(s[1]);
     }
 }
