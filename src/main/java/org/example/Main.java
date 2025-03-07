b20ffe6012c7b459a16970542edb766c5d41dd2d
Yullc <apanan0910@gmail.com>
2025-03-07 16:49:11 +0900

    day4 배열 연습해보기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,30 +6,30 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        String[] time = str.split("");
-        if(str.length()==8){
-            System.out.println(time.length);
-            if (time[3]=="0"&&time[4]=="0"){
-                System.out.println("0");
-            }
-            else{
-                System.out.println(time[3]+time[4]);
-            }
-        }
-        else{
-            System.out.println(time.length);
-            System.out.println(time[0]+time[6]);
-            if (time[2]=="0"&&time[3]=="0"){
-
-                System.out.println("0");
-            }
-            else {
-                System.out.println(time[2]+time[3]);
-                System.out.println(time[2]+time[3]);
-            }
-        }
 
     }
 }
+
+
+// //int[] arr= new(객체1개를 만들겠다) 설계도)
+//
+//        //int[] arr= new int[3];
+//        //int[] arr2= arr;    주소복사
+//        //지역변수는 함수가 끝나면 종료
+//        //객체는 gc에 의해 제거
+//
+//        int[] arr; //'정수들(int[])' 모양으로 만들어진 객체의 리모컨만 들어갈 수 있는 arr변수 생성
+//        arr = new int[4]; // arr이라는 변수에 int[4](설계도)로 만든 객체의 리모컨을 넣겠다
+
+//boolean arr1[] = new boolean[]{true, false, true};
+//        System.out.print(arr1[0]);
+//        System.out.print(arr1[1]);
+//        System.out.println(arr1[2]);
+// double arr2[] = new double[] {3.14,7.77,11.11};
+//            System.out.println(Arrays.toString(arr2));
+
+// int arr3[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        for (int i = 0; i < arr3.length; i++) {
+//            arr3[i]=1+i;
+//            System.out.println(arr3[i]);
+//        }
