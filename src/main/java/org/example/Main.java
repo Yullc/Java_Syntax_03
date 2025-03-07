7eec95cdba846dab03a5b922825d95e1731604d6
Yullc <apanan0910@gmail.com>
2025-03-07 09:57:05 +0900

    day4 codeUp 문제 실패

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,13 +9,27 @@ class Main {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         String[] time = str.split("");
-        if(str.length()==7){
-            time[2]
-            System.out.println(time[2]+time[3]);
+        if(str.length()==8){
+            System.out.println(time.length);
+            if (time[3]=="0"&&time[4]=="0"){
+                System.out.println("0");
+            }
+            else{
+                System.out.println(time[3]+time[4]);
+            }
         }
         else{
-            System.out.println(time[3]+time[4]);
+            System.out.println(time.length);
+            System.out.println(time[0]+time[6]);
+            if (time[2]=="0"&&time[3]=="0"){
+
+                System.out.println("0");
+            }
+            else {
+                System.out.println(time[2]+time[3]);
+                System.out.println(time[2]+time[3]);
+            }
         }
-       ;
+
     }
 }
