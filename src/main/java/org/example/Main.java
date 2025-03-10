49bc9ac8a701ccb487b8feceb85c1ea71f3df667
Yullc <apanan0910@gmail.com>
2025-03-10 16:16:42 +0900

    day5 클래스 공부

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,32 @@
 package org.example;
 
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
-        //        int[] aa = new int[3];
-        //        aa[0] = "김철수";
-        //        aa[1] = 23;
-        //        aa[2] 165.3;
+        사람 a =new 사람();
+        a.age =23;
+        a.name ="김철수";
+        a.isMarried=true;
+        a.introduce();
 
-        //        (만들어진)객체 == 인스턴스
-        //객체안에 있는 변수처럼 동작하는 공간 == 인스턴스변수
-        //다양한 타입을 저장할 수 있는 사용자 정의 객체를 만들고 싶어
-        chulsoo a = new chulsoo();
-        a.name = "김철수";
-        a.age = 23;
-        a.tall = 165.3;
+        사람 b =new 사람();
+        b.age=24;
+        b.name="박영수";
+        b.isMarried = true;
+        b.introduce();
     }
 }
-class chulsoo {
-    String name;
+class 사람{
     int age;
-    double tall;
-}
+    String name;
+    boolean isMarried;
 
+    void introduce(){
+        System.out.println("==자기소개==");
+        System.out.println("나이: "+this.age);
+        System.out.println("이름: "+this.name);
+        System.out.println("결혼여부: " +this.isMarried);
+
+    }
+
+}
