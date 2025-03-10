040b64d09e63224d71310f3fe6b7e08b6e9131ca
Yullc <apanan0910@gmail.com>
2025-03-10 11:47:50 +0900

    day5 클래스

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,30 +6,23 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        //        int[] aa = new int[3];
+        //        aa[0] = "김철수";
+        //        aa[1] = 23;
+        //        aa[2] 165.3;
 
+        //        (만들어진)객체 == 인스턴스
+        //객체안에 있는 변수처럼 동작하는 공간 == 인스턴스변수
+        //다양한 타입을 저장할 수 있는 사용자 정의 객체를 만들고 싶어
+        chulsoo a = new chulsoo();
+        a.name = "김철수";
+        a.age = 23;
+        a.tall = 165.3;
     }
 }
+class chulsoo {
+    String name;
+    int age;
+    double tall;
+}
 
-
-// //int[] arr= new(객체1개를 만들겠다) 설계도)
-//
-//        //int[] arr= new int[3];
-//        //int[] arr2= arr;    주소복사
-//        //지역변수는 함수가 끝나면 종료
-//        //객체는 gc에 의해 제거
-//
-//        int[] arr; //'정수들(int[])' 모양으로 만들어진 객체의 리모컨만 들어갈 수 있는 arr변수 생성
-//        arr = new int[4]; // arr이라는 변수에 int[4](설계도)로 만든 객체의 리모컨을 넣겠다
-
-//boolean arr1[] = new boolean[]{true, false, true};
-//        System.out.print(arr1[0]);
-//        System.out.print(arr1[1]);
-//        System.out.println(arr1[2]);
-// double arr2[] = new double[] {3.14,7.77,11.11};
-//            System.out.println(Arrays.toString(arr2));
-
-// int arr3[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-//        for (int i = 0; i < arr3.length; i++) {
-//            arr3[i]=1+i;
-//            System.out.println(arr3[i]);
-//        }
