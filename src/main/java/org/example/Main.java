129921a1f86db41136e27de43b2470c9eaf6321f
Yullc <apanan0910@gmail.com>
2025-03-13 14:36:17 +0900

    03.13 39강 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,58 +1,59 @@
 package org.example;
-
-
-//올바른 리턴타입으로 메서드를 만들어주세요
-public class Main {
+class Main {
     public static void main(String[] args) {
-        int i = 로봇.get정수();
-        boolean b = 로봇.get논리();
-        사람 a사람 = 로봇.get사람();
-        사람 a사람2 = 로봇.get사람2();
-        사람 a사람3 = 로봇.get사람3();
-        사람 a사람4 = 로봇.get사람4();
-        사람 a사람5 = 로봇.get사람5(123,false);
-        로봇.get사람6(123,false);
+        전사 a전사 = new 전사();
 
+        String 이름 = "칸";
+        a전사.이름 = 이름;
+        a전사.나이 = 20;
+        a전사.자기소개();
+
+        a전사.나이++;
+        a전사.자기소개();
+
+        a전사.나이 = 30;
+        a전사.이름 = "카니";
+        a전사.자기소개();
+
+        a전사.a무기 = new 활();
+        a전사.공격();
+        // 출력 : 카니가 활로 공격합니다.
+
+        a전사.a무기 = new 칼();
+        a전사.공격();
+        // 출력 : 카니가 칼로 공격합니다.
     }
-
 }
-class 사람{
 
-    int 학번;
-    boolean 출결;
+class 전사 {
+    // 인스턴스 변수
+    String 이름;
+    // 인스턴스 변수
+    int 나이;
+    // 인스턴스 변수
+    무기 a무기;
+
+    void 자기소개() {
+        System.out.println("안녕하세요. 저는 " + this.나이 + "살 " + this.이름 + " 입니다.");
+    }
+
+    void 공격() {
+        a무기.작동(this.이름);
+    }
 }
-class 로봇{
-    public static int get정수() {
-        return 0;
-    }
-    public static boolean get논리(){
-        return true;
-    }
 
-    public static 사람 get사람() {
-        return null;
-    }
-    public static 사람 get사람2() {
-        return new 사람();
-    }
+class 무기 {
+   void 작동(String 공격자_이름){};
+}
 
-    public static 사람 get사람3() {
-        사람 a사람 = null;
-        return a사람;
-    }
+class 칼 extends 무기 {
+   void 작동(String 공격자_이름){
+       System.out.println(공격자_이름+"가 칼로 공격합니다");
+   }
+}
 
-    public static 사람 get사람4() {
-        사람 a사람 = new 사람();
-        return a사람;
-    }
-    public static 사람 get사람5(int 학번, boolean 출결){
-        사람 a사람 = new 사람();
-        a사람.학번 = 학번;
-        a사람.출결 = 출결;
-        return a사람;
-    }
-
-    public static void get사람6(int i, boolean a) {
-
+class 활 extends 무기 {
+    void 작동(String 공격자_이름){
+        System.out.println(공격자_이름+"가 활로 공격합니다");
     }
 }
