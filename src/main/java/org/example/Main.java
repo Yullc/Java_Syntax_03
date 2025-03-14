85651f07f01b74889019f56a3019aef5e87e930b
Yullc <apanan0910@gmail.com>
2025-03-14 10:38:27 +0900

    03.14 배열객체

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,57 +2,44 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-//        int[] arr = new int [3];
-//        Article article = new Article[3];
-//        int lastId=0;
-//
-//        Article article1 = new Article();
-//        lastId++;
-//        article1.id = lastId;
-//        System.out.println(article1.id);
-//
-//        Article article2 = new Article();
-//        lastId++;
-//        article2.id = lastId;
-//        System.out.println(article1.id);
-//
-//        Article article3 = new Article();
-//        lastId++;
-//        article3.id = lastId;
-//        System.out.println(article1.id);
-//    }
-        int lastId=0;
+
+        int[] arr = new int[10];
+
+        Article[] articles = new Article[10];
 
         Article article1 = new Article();
-
-        System.out.println(article1.id);
+        articles[0] = article1;
 
         Article article2 = new Article();
+        articles[1] = article2;
 
+        articles[2] = new Article();
+        System.out.println(articles[0].id);
+        System.out.println(articles[1].id);
+        System.out.println(articles[2].id);
+
+        System.out.println(article1.id);
         System.out.println(article2.id);
-
     }
 }
-class Article{
+
+class Article {
+    static int lastId;
+
     int id;
-    String regData;
+    String regDate;
 
-    static int lastId; // 클래스에 속한 클래스 변수 - 모든 인스턴스 공유, 객체 생성과 무관
-
-    static { // static 전용 생성자
-        lastId=0;
+    static {
+        lastId = 0;
     }
 
-    Article(){
-        this(lastId+1,"2025-12-12 12:12:12");// 이건 꼭 생성자 바디 첫줄에 있어야 함 // 다른 생성자 호출 실행!
-        // 생성자 안에서 다른 생성자 호출 가능
+    Article() {
+        this(lastId + 1, "2025-12-12 12:12:12"); // 다른 생성자 호출(실행), Constructor Call
         lastId++;
-        //this.id =1;
     }
-    
 
-    Article(int id, String regData){
+    Article(int id, String regDate) {
         this.id = id;
-        this.regData = regData;
+        this.regDate = regDate;
     }
 }
