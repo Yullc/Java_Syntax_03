0fdebf7746a33cfb283d57d942f87dba63863649
Yullc <apanan0910@gmail.com>
2025-03-14 10:20:01 +0900

    03.14 생성자

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,59 +1,58 @@
 package org.example;
-class Main {
+
+public class Main {
     public static void main(String[] args) {
-        전사 a전사 = new 전사();
+//        int[] arr = new int [3];
+//        Article article = new Article[3];
+//        int lastId=0;
+//
+//        Article article1 = new Article();
+//        lastId++;
+//        article1.id = lastId;
+//        System.out.println(article1.id);
+//
+//        Article article2 = new Article();
+//        lastId++;
+//        article2.id = lastId;
+//        System.out.println(article1.id);
+//
+//        Article article3 = new Article();
+//        lastId++;
+//        article3.id = lastId;
+//        System.out.println(article1.id);
+//    }
+        int lastId=0;
 
-        String 이름 = "칸";
-        a전사.이름 = 이름;
-        a전사.나이 = 20;
-        a전사.자기소개();
+        Article article1 = new Article();
 
-        a전사.나이++;
-        a전사.자기소개();
+        System.out.println(article1.id);
 
-        a전사.나이 = 30;
-        a전사.이름 = "카니";
-        a전사.자기소개();
+        Article article2 = new Article();
 
-        a전사.a무기 = new 활();
-        a전사.공격();
-        // 출력 : 카니가 활로 공격합니다.
+        System.out.println(article2.id);
 
-        a전사.a무기 = new 칼();
-        a전사.공격();
-        // 출력 : 카니가 칼로 공격합니다.
     }
 }
+class Article{
+    int id;
+    String regData;
 
-class 전사 {
-    // 인스턴스 변수
-    String 이름;
-    // 인스턴스 변수
-    int 나이;
-    // 인스턴스 변수
-    무기 a무기;
+    static int lastId; // 클래스에 속한 클래스 변수 - 모든 인스턴스 공유, 객체 생성과 무관
 
-    void 자기소개() {
-        System.out.println("안녕하세요. 저는 " + this.나이 + "살 " + this.이름 + " 입니다.");
+    static { // static 전용 생성자
+        lastId=0;
     }
 
-    void 공격() {
-        a무기.작동(this.이름);
+    Article(){
+        this(lastId+1,"2025-12-12 12:12:12");// 이건 꼭 생성자 바디 첫줄에 있어야 함 // 다른 생성자 호출 실행!
+        // 생성자 안에서 다른 생성자 호출 가능
+        lastId++;
+        //this.id =1;
     }
-}
+    
 
-class 무기 {
-   void 작동(String 공격자_이름){};
-}
-
-class 칼 extends 무기 {
-   void 작동(String 공격자_이름){
-       System.out.println(공격자_이름+"가 칼로 공격합니다");
-   }
-}
-
-class 활 extends 무기 {
-    void 작동(String 공격자_이름){
-        System.out.println(공격자_이름+"가 활로 공격합니다");
+    Article(int id, String regData){
+        this.id = id;
+        this.regData = regData;
     }
 }
--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -1,30 +1,23 @@
-package org.example;
-
-import java.util.Arrays;
-
-public class  test {
-    public static void main(String[] args) {
-
-        Solution s = new Solution();
-
-        System.out.println(s.solution("abcdef","f"));
-    }
-
-
-}
-class Solution {
-    public String solution(String my_string, String letter) {
-        String answer = "";
-        String arr[] = new String[my_string.length()];
-        String arr1[] = new String[letter.length()];
-
-        for(int i = 0; i < my_string.length(); i++) {
-            if (arr[i] == arr1[0]) {
-                answer = arr[i];
-                System.out.println(answer);
-            }
-            return answer;
-        }
-        return answer;
-    }
-}
+//package org.example;
+//
+//import java.util.Arrays;
+//
+//public class  test {
+//    public static void main(String[] args) {
+//        Solution s = new Solution();
+//        System.out.println(s.solution("hello",3));
+//    }
+//
+//
+//}
+//class Solution {
+//    public String solution(String my_string, int n) {
+//        String answer = "";
+//        String [] arr = {my_string};
+//        for(int i=n; i<=arr.length;){
+//            answer = arr[i];
+//            System.out.println(answer);
+//        }
+//        return answer;
+//    }
+//}
