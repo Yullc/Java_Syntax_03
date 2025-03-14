d4359f17c652d0976cea9dd97e3fe1efdb9dadc3
Yullc <apanan0910@gmail.com>
2025-03-14 16:49:35 +0900

    03.14 과제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,45 +1,60 @@
 package org.example;
+// 문제 : 아래가 실행되도록 해주세요.
+// 조건 : 배열을 사용할 수 없습니다.
 
-public class Main {
+class Main {
     public static void main(String[] args) {
+        사람인력관리소 a사람인력관리소 = new 사람인력관리소();
 
-        int[] arr = new int[10];
+        a사람인력관리소.add사람("홍길순", 33);
+        // 나이가 33살인 1번째 사람(홍길순)이 추가되었습니다.
+        a사람인력관리소.add사람("홍길동", 20);
+        // 나이가 20살인 2번째 사람(홍길동)이 추가되었습니다.
+        a사람인력관리소.add사람("임꺽정", 30);
+        // 나이가 30살인 3번째 사람(임꺽정)이 추가되었습니다.
 
-        Article[] articles = new Article[10];
+        사람 a사람1 = a사람인력관리소.get사람(1);
+        a사람1.자기소개();
+        // 저는 1번, 홍길순, 33살 입니다.
 
-        Article article1 = new Article();
-        articles[0] = article1;
+        사람 a사람2 = a사람인력관리소.get사람(2);
+        a사람2.자기소개();
+        // 저는 2번, 홍길동, 20살 입니다.
 
-        Article article2 = new Article();
-        articles[1] = article2;
-
-        articles[2] = new Article();
-        System.out.println(articles[0].id);
-        System.out.println(articles[1].id);
-        System.out.println(articles[2].id);
-
-        System.out.println(article1.id);
-        System.out.println(article2.id);
+        사람 a사람3 = a사람인력관리소.get사람(3);
+        a사람3.자기소개();
+        // 저는 3번, 임꺽정, 30살 입니다.
     }
 }
+class 사람인력관리소 {
+    사람 a사람인력관리소;
 
-class Article {
-    static int lastId;
+    String name;
+    int age;
+    void add사람(String name, int age){
+        this.age = age;
+        this.name= name;
 
-    int id;
-    String regDate;
-
-    static {
-        lastId = 0;
+        System.out.println("나이가 " +this.age+"인 1번째 사람"+this.name+"이 추가되었습니다");
     }
 
-    Article() {
-        this(lastId + 1, "2025-12-12 12:12:12"); // 다른 생성자 호출(실행), Constructor Call
-        lastId++;
+    public 사람인력관리소(int age, String name) {
+        this.age = age;
+        this.name = name;
     }
 
-    Article(int id, String regDate) {
-        this.id = id;
-        this.regDate = regDate;
+
+    public 사람 get사람(int i) {
+
+        return ;
     }
+}
+class 사람 extends {
+    사람 a사람인력관리소;
+
+   
+
+    void 자기소개(){
+
+     }
 }
